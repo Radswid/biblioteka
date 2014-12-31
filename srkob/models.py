397a06e2e40ff1088c28318334fd215cbdc51bc9
Krupski Grzegorz <krupskig@gmail.com>
2014-12-31 17:46:10 +0100

    Modele
    Dodałem kilka modeli nie wszystkie jeszcze

--- srkob/models.py
+++ srkob/models.py
@@ -1,3 +1,44 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from rango.models import UserProfile
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+class author(models.Model):
+    first_name = models.CharField('Imię', max_length=50)
+    last_name = models.CharField('Nazwisko', max_length=50)
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autorzy"
+
+    def __unicode__(self):
+        return self.last_name
+    
+class books(models.Model):
+    title = models.CharField('Tytuł', max_length=50)
+    slug = models.SlugField('Odnośnik', unique=True, max_length=100)
+    author = models.ForeignKey('Autor', author)
+    about = models.TextField('Opis')
+    state = models.BooleanField('Wypożyczona')
+
+    class Meta:
+        verbose_name = "Książka"
+        verbose_name_plural = "Książki"
+
+    def __unicode__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    address = model.TextField('Adres')
+    p_number = model.CharField('Pesel', max_length=11)
+    
+    class Meta:
+        verbose_name = "Profil"
+        verbose_name_plural = "Profile"
+
+    def __unicode__(self):
+        return self.user.username
+    
+    
